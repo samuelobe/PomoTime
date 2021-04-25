519efbc459b740bf4c42fb7f78ac6563656bf854
Samuel Obe <samuel.obe@outlook.com>
2021-04-25 19:38:12 -0400

    Initial Commit

---
+++ PomoTime/PomoTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoTimeApp.swift
+//  PomoTime
+//
+//  Created by Sam Obe on 4/25/21.
+//
+
+import SwiftUI
+
+@main
+struct PomoTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
